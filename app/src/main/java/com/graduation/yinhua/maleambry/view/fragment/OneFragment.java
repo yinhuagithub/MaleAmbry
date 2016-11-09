5af01beb315b8e54771b5d3eb2f76b92892c93c9
yinhuagithub <1004122124@qq.com>
2016-11-09 11:09:32 +0800

    使用TabLayout实现Bottom Navigation Layout效果

---
+++ app/src/main/java/com/graduation/yinhua/maleambry/view/fragment/OneFragment.java
@@ -0,0 +1,31 @@
+package com.graduation.yinhua.maleambry.view.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.graduation.yinhua.maleambry.R;
+
+/**
+ * OneFragment.java
+ * Description:
+ * <p/>
+ * Created by yinhua on 2016/11/9.
+ * git：https://github.com/yinhuagithub/MaleAmbry
+ */
+public class OneFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_one,container,false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
