5c81d22510ff2ddd418e8fc7a7f17ff8c36ad961
yinhuagithub <1004122124@qq.com>
2016-11-22 14:37:50 +0800

    still progress bar实现

---
+++ app/src/main/java/com/graduation/yinhua/maleambry/utils/ReflectUtils.java
@@ -0,0 +1,60 @@
+package com.graduation.yinhua.maleambry.utils;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+/**
+ * ReflectUtils.java
+ * Description:
+ * <p/>
+ * Created by yinhua on 2016/11/22.
+ * git：https://github.com/yinhuagithub/MaleAmbry
+ */
+public class ReflectUtils {
+    private ReflectUtils() {
+    }
+
+    /**
+     * 直接设置对象属性值,无视private/protected修饰符,不经过setter函数.
+     */
+    public static void setFieldValue(final Object object, final String fieldName, final Object value) {
+        Field field = getDeclaredField(object, fieldName);
+        if (field == null)
+            throw new IllegalArgumentException("Could not find field [" + fieldName + "] on target [" + object + "]");
+        makeAccessible(field);
+        try {
+            field.set(object, value);
+        } catch (IllegalAccessException e) {
+        }
+    }
+
+    /**
+     * 循环向上转型,获取对象的DeclaredField.
+     */
+    protected static Field getDeclaredField(final Object object, final String fieldName) {
+        return getDeclaredField(object.getClass(), fieldName);
+    }
+
+    /**
+     * 循环向上转型,获取类的DeclaredField.
+     */
+    @SuppressWarnings("unchecked")
+    protected static Field getDeclaredField(final Class clazz, final String fieldName) {
+        for (Class superClass = clazz; superClass != Object.class; superClass = superClass.getSuperclass()) {
+            try {
+                return superClass.getDeclaredField(fieldName);
+            } catch (NoSuchFieldException e) {
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 强制转换field可访问.
+     */
+    protected static void makeAccessible(Field field) {
+        if (!Modifier.isPublic(field.getModifiers()) || !Modifier.isPublic(field.getDeclaringClass().getModifiers())) {
+            field.setAccessible(true);
+        }
+    }
+}
